aefcc2e3da752d22b7d088c1949e2a826917e0b3
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2021-06-01 13:59:53 -0500

    Refactor injector package name for di

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'Movies/injector/bloc_injector.dart';
-import 'Movies/injector/bloc_module.dart';
+import 'Movies/di/bloc_injector.dart';
+import 'Movies/di/bloc_module.dart';
 
 void main() async {
   var container = await BlocInjector.create(BlocModule());
