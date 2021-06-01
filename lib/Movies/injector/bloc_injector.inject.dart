07bbc39726aa6ffe6ce38135e036aee0b8c326d3
alejandra.zuleta <alejandra.zuleta@sofka.com.co>
2021-06-01 13:47:05 -0500

    Refactor deleted detail image

--- lib/Movies/injector/bloc_injector.inject.dart
+++ lib/Movies/injector/bloc_injector.inject.dart
@@ -6,7 +6,6 @@ import '../repository/repository.dart' as _i5;
 import 'dart:async' as _i6;
 import '../movies_app.dart' as _i7;
 import '../bloc/movies_bloc.dart' as _i8;
-//import '../bloc/movie_detail_bloc.dart' as _i9;
 
 class BlocInjector$Injector implements _i1.BlocInjector {
   BlocInjector$Injector._(this._blocModule);
@@ -24,13 +23,11 @@ class BlocInjector$Injector implements _i1.BlocInjector {
     return injector;
   }
 
-//  _i7.MoviesApp _createApp() => _i7.MoviesApp(_createMoviesBloc(), _createMovieDetailBloc());
   _i7.MoviesApp _createApp() => _i7.MoviesApp(_createMoviesBloc());
   _i8.MoviesBloc _createMoviesBloc() => _i8.MoviesBloc(_createRepository());
   _i5.Repository _createRepository() => _singletonRepository ??= _blocModule.repository(_createMovieApiProvider());
   _i4.MovieApiProvider _createMovieApiProvider() => _singletonMovieApiProvider ??= _blocModule.movieApiProvider(_createClient());
   _i3.Client _createClient() => _singletonClient ??= _blocModule.client();
-  //_i9.MovieDetailBloc _createMovieDetailBloc() =>_i9.MovieDetailBloc(_createRepository());
 
   @override
   _i7.MoviesApp get app => _createApp();
